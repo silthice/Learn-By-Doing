75971c13842105de67607472271fdb6980a8022d
Giap <ryon@conjurers.dev>
2023-01-18 10:04:01 +0800

    - Git Init

---
+++ Learn By Doing/Learn_By_DoingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_By_DoingApp.swift
+//  Learn By Doing
+//
+//  Created by Giap on 18/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct Learn_By_DoingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
